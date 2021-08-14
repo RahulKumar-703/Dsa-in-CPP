e1b88992013b051a46025c10b42dea0b5365206c
Rahul <RahulKumar703@outlook.com>
2021-08-14 11:06:40 +0530

    Added Comments in 3rd array problem 'BinarySearchRecursion'

--- Array/BinarySearchRecursion.cpp
+++ Array/BinarySearchRecursion.cpp
@@ -4,10 +4,11 @@ using std::cout;
 int binarySearch(int element, int array[], int start, int end)
 {
 
+    // base Condition
     if(start >= end)
     {
-        if(array[start]==element) return start;
-        else return -1;
+        if(array[start]==element) return start; //Return index if found
+        else return -1; // Return -1 if not found
     }
     else
     {
@@ -16,7 +17,7 @@ int binarySearch(int element, int array[], int start, int end)
         int mid = start + (end - start)/2;  // It is not adding directly start and end.
 
         if(array[mid] == element) return mid;
-        else if (array[mid] > element) return binarySearch(element, array, start, mid-1);
+        else if (array[mid] > element) return binarySearch(element, array, start, mid-1);   // if element at mid index is graeter than desired element then discard the half array(of smaller side) and again search in another half
         else return binarySearch(element, array, mid+1, end);
 
     }
@@ -26,6 +27,8 @@ int main()
 {
 
     int arr[] = {1,2,4,6,7,11,21};
+
+    // Passed parameters as element, arrray, startting index of array, ending index of array (size)
 
     cout << binarySearch(-11, arr, 0, sizeof(arr)/sizeof(arr[0]));
 
--- README.md
+++ README.md
@@ -19,6 +19,6 @@ MY Journy Of Learning __*Data Structure And Algorithm*__
 ## Array
 
 
-  1. > [Find An Element From An Array (Linear Search)](https://github.com/RahulKumar-703/Dsa-in-CPP/blob/main/Array/FindElement.cpp) 
-  2. > [Find An Element From An Sorted Array (Binary Search)](https://github.com/RahulKumar-703/Dsa-in-CPP/blob/main/Array/BinarySearch.cpp)
-  3. > [Find An Element From An Sorted Array (Binary Search Using Recursion)](https://github.com/RahulKumar-703/Dsa-in-CPP/blob/main/Array/BinarySearchRecursion.cpp)
+  1. > [Find An Element From An Array `(Linear Search)`](https://github.com/RahulKumar-703/Dsa-in-CPP/blob/main/Array/FindElement.cpp) 
+  2. > [Find An Element From An Sorted Array `(Binary Search)`](https://github.com/RahulKumar-703/Dsa-in-CPP/blob/main/Array/BinarySearch.cpp)
+  3. > [Find An Element From An Sorted Array `(Binary Search Using Recursion)`](https://github.com/RahulKumar-703/Dsa-in-CPP/blob/main/Array/BinarySearchRecursion.cpp)
